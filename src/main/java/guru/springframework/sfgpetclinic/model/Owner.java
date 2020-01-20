bbf2e4c3b1bc3f5e24783d3d80715523f90eb13b
Jgayle <Jgayle99@gmail.com>
2020-01-20 10:10:11 -0500

    Added POJO models. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by Joel Gayle on 20 Jan, 2020
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by Joel Gayle on 20 Jan, 2020
+ */
+public class Vet extends Person{
+
+}

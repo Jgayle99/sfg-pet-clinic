d7f6f8cfe6874e9eed154b54811b84247b522908
Jgayle <Jgayle99@gmail.com>
2020-01-24 22:27:50 -0500

    Fixed code back to current lesson, was out of synch with lessons.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,26 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.*;
-
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by Joel Gayle on 20 Jan, 2020
  */
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-@Table(name = "vets")
 public class Vet extends Person {
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
-            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
-    private Set<Specialty> specialities = new HashSet<>();
-
 }
